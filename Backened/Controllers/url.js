2d0d04b4684c9a4bdc00fb646ccd506f1511003e
Ibrahim1249 <mominibrahim98.im85@gmail.com>
2024-09-05 18:54:28 +0530

    added handlepost function in controller

--- Backened/Controllers/url.js
+++ Backened/Controllers/url.js
@@ -1,15 +1,40 @@
-const shortID = require("shortid")
+const shortID = require("shortid");
+const urlModel = require("../Models/urlModel");
 
-function handlePostURL(req,res){
+async function handlePostURL(req,res){
     const body = req.body;
     if(!body.url) {
          res.status(400).json({error : "URL must be their !!"})
     } 
-    const id = shortID();
-    console.log(req.body.url , id , req.userCity , req.deviceInfo)
-    res.status(201).json({message : "URL is added to DB"})
-}
+    try{
+        const short_id = shortID();
+        
+        // here i am checking the if i already have same URL that their is not point in adding on db instead i return short_URL correspond to that URL 
+        const existingUrl = await urlModel.findOne({original_URL : req.body.url})
 
+        if(existingUrl){
+            return res.status(200).json({message : "URL is Already existed" , userShortId : existingUrl.short_URL})
+        }
+
+        // here i am creating new URL for user
+        const newUrl = new urlModel({
+            original_URL : req.body.url,
+            short_URL : short_id,
+            totalClicks : 0,
+            visits : [{
+                city : req.userCity || "unknown" ,
+                device : req.deviceInfo.name || "unknown",
+                timestamp : Date.now()
+            }]
+        })
+        await newUrl.save()
+        res.status(201).json({message : "URL is added to DB" , userShortId : short_id})
+        
+    }catch (error) {
+        console.error('Error saving URL:', error);
+        res.status(500).json({ error: "Error saving URL to database" });
+    }
+}
 
 module.exports = {
     handlePostURL
--- Backened/index.js
+++ Backened/index.js
@@ -1,15 +1,21 @@
 const express = require("express");
 const urlRouter = require("./Routers/urlRouter");
+const { default: mongoose } = require("mongoose");
 const app = express();
 const PORT = 6969
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
+mongoose.connect("mongodb://127.0.0.1:27017/geekster").then(()=>{
+    app.listen(PORT,()=>{
+        console.log("server running on port" , PORT)
+    })
+}).catch((error)=>{
+    console.log(error.message)
+})
+
 
 // router
 app.use("/url",urlRouter);
 
-app.listen(PORT,()=>{
-    console.log("server running on port" , PORT)
-})
